6c34896361241cf189209c401ee5bd6582befe67
Rumusbin <komlikov.r@gmail.com>
2020-09-20 18:16:09 +0300

    Initial commit

---
+++ app/app/Http/Controllers/IndexController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Routing\Controller as BaseController;
+
+class IndexController extends BaseController
+{
+    public function index()
+    {
+        dd(123);
+    }
+
+}
