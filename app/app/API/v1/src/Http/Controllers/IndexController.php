7c2a129a444c34d25eed1bc59e8178157b302587
Rumusbin <komlikov.r@gmail.com>
2020-10-11 19:46:33 +0300

    API strcture project build begin

---
+++ app/app/API/v1/src/Http/Controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace API\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function index()
+    {
+        dd(12553);
+    }
+}
--- app/config/app.php
+++ app/config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
 
+        /*
+         *  Api Service providers ...
+         */
+        API\ApiServiceProvider::class
+
     ],
 
     /*
